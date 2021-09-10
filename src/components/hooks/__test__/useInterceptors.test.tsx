59c1944da40b0758b06b6b62ab819b7a3dc4f240
Zhang Rui <zhangrui@growingio.com>
2021-09-10 13:37:22 +0800

    test(hooks): add hooks unit testing by using tsx

--- src/components/hooks/__test__/useInterceptors.test.tsx
+++ src/components/hooks/__test__/useInterceptors.test.tsx
@@ -1,14 +1,51 @@
 import { Chart } from '@antv/g2';
 import { renderHook, act } from '@testing-library/react-hooks';
+import EventEmitter from '@antv/event-emitter';
 import useInterceptors from '../useInterceptors';
 
 describe('test useInterceptors', () => {
-  test('test', () => {
+  test('test interceptors onRender', () => {
     const { result } = renderHook(() => useInterceptors());
     const chartObj = {};
     act(() => {
       result.current.interceptors.onRender(chartObj as Chart);
     });
     expect(result.current.getCharts()).toEqual([chartObj]);
+  });
+
+  test('test interceptors bindElementEvents', () => {
+    const { result } = renderHook(() => useInterceptors());
+
+    const chartObj = new EventEmitter();
+    (chartObj as any).lockTooltip = jest.fn;
+    (chartObj as any).unlockTooltip = jest.fn;
+    act(() => {
+      result.current.interceptors.bindElementEvents(chartObj as Chart);
+    });
+
+    act(() => {
+      chartObj.emit('element:click');
+    });
+    expect(result.current.getTriggerAction()).toBe('click');
+
+    act(() => {
+      chartObj.emit('element:mouseover');
+    });
+    expect(result.current.getTriggerAction()).toBe('mouseover');
+
+    act(() => {
+      chartObj.emit('element:mouseout', { event: { relatedTarget: false } });
+    });
+    expect(result.current.getTriggerAction()).toBe('mouseover');
+
+    act(() => {
+      chartObj.emit('element:click');
+    });
+    expect(result.current.getTriggerAction()).toBe('click');
+
+    act(() => {
+      chartObj.emit('element:mouseout', { event: { relatedTarget: true } });
+    });
+    expect(result.current.getTriggerAction()).toBe('mouseover');
   });
 });
---
+++ src/components/hooks/__test__/useLegends.test.tsx
@@ -0,0 +1,73 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useLegends, { getLegends } from '../useLegends';
+import { ChartType, Legends } from '../../../interface';
+
+describe('test useLegends', () => {
+  const testLegends = {
+    北京: { name: '北京', color: '#5F87FF', active: true, type: 'bar' },
+    上海: { name: '上海', color: '#FFDD63', active: true, type: 'bar' },
+  };
+
+  const lineDashLgends = {
+    南京: {
+      name: '南京',
+      lineDash: [1, 2, 3],
+      color: '#62CE6C',
+      active: true,
+      type: 'bar',
+    },
+  };
+  const fullLegends = { ...testLegends, ...lineDashLgends };
+  test('test getLegends', () => {
+    const [legends, hasDashed] = getLegends(ChartType.BAR, [
+      '北京',
+      { name: '上海' },
+      { name: '南京', lineDash: [1, 2, 3] },
+    ]);
+    expect(legends).toEqual(fullLegends);
+    expect(hasDashed).toBeFalsy();
+
+    const [nullLegends] = getLegends(ChartType.COLUMN, undefined as any);
+    expect(nullLegends).toEqual({});
+  });
+
+  test('test useLegends hook', () => {
+    const { result } = renderHook(() => useLegends());
+
+    act(() => {
+      result.current.setLegends(fullLegends as Legends);
+    });
+    expect(result.current.legends).toEqual(fullLegends);
+
+    act(() => {
+      result.current.updateLegends('南京');
+    });
+    expect(result.current.legends).toEqual({ ...testLegends, 南京: { ...lineDashLgends['南京'], active: false } });
+
+    // if the legends is empty object {}
+    act(() => {
+      result.current.setLegends({});
+    });
+    act(() => {
+      result.current.updateLegends('南京');
+    });
+    expect(result.current.legends).toEqual({
+      南京: {
+        active: true,
+      },
+    });
+
+    // if the legends is undefined
+    act(() => {
+      result.current.setLegends(undefined as any);
+    });
+    act(() => {
+      result.current.updateLegends('南京');
+    });
+    expect(result.current.legends).toEqual({
+      南京: {
+        active: true,
+      },
+    });
+  });
+});
