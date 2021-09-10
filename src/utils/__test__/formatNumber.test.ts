a60bf774cba3471e0799e4c75d9e1ff1cb2b8bb2
chen_gh <chenguanghao@growingio.com>
2021-09-10 17:39:50 +0800

    feat: add formatNumber test

--- src/utils/__test__/formatNumber.test.ts
+++ src/utils/__test__/formatNumber.test.ts
@@ -1,67 +1,83 @@
 import { formatNumber, parseFormatedNumber, formatPercent, formatTemperature } from '../formatNumber';
 
 describe('test formatNumber', () => {
-  test('test formatNumber', () => {
+  test('formatNumber', () => {
     expect(formatNumber(20)).toBe('20');
     expect(formatNumber(123415454)).toBe('123,415,454');
     expect(formatNumber(123415454.23)).toBe('123,415,454.23');
+    expect(formatNumber(123415454.23232332)).toBe('123,415,454.23');
+    expect(formatNumber(0.23232332)).toBe('0.23');
   });
 
-  test('test formatNumber with finite value', () => {
+  test('formatNumber decimalCount', () => {
+    expect(formatNumber(0.23, 0)).toBe('0');
+    expect(formatNumber(0.26123123, 1)).toBe('0.3');
+    expect(formatNumber(123415454.23, 0)).toBe('123,415,454');
+    expect(formatNumber(123415454.6, 0)).toBe('123,415,455');
+  });
+
+  test('formatNumber decimalCount intSuffixZeroFill', () => {
+    expect(formatNumber(0.6, 3, true)).toBe('0.6');
+    expect(formatNumber(123415454.6, 3, true)).toBe('123,415,454.6');
+    expect(formatNumber(0, 3, true)).toBe('0.000');
+    expect(formatNumber(123415454, 3, true)).toBe('123,415,454.000');
+  });
+
+  test('formatNumber with finite value', () => {
     expect(formatNumber(Infinity)).toEqual('Infinity');
   });
 
-  test('test formatNumber with percent', () => {
+  test('formatNumber with percent', () => {
     expect(formatNumber('30%')).toEqual('30%');
   });
 
-  test('test formatNumber with -', () => {
+  test('formatNumber with -', () => {
     expect(formatNumber(-1234)).toEqual('-1,234');
   });
 
-  test('test formatNumber with demical number', () => {
+  test('formatNumber with demical number', () => {
     expect(formatNumber(0)).toEqual('0');
   });
 
-  test('test formatNumber with more settings', () => {
+  test('formatNumber with more settings', () => {
     expect(formatNumber(0, 3, true)).toEqual('0.000');
   });
 });
 
 describe('test formatTemperature', () => {
-  test('test formatTemperature', () => {
+  test('formatTemperature', () => {
     expect(formatTemperature(20)).toBe('20 °C');
   });
 
-  test('test formatTemperature with more settings', () => {
+  test('formatTemperature with more settings', () => {
     expect(formatTemperature(0.2, 3, true)).toBe('0.2 °C');
   });
 
-  test('test formatTemperature with string', () => {
+  test('formatTemperature with string', () => {
     expect(formatTemperature('20 °C')).toBe('20 °C');
   });
 });
 
 describe('test formatPercent', () => {
-  test('test formatPercent', () => {
+  test('formatPercent', () => {
     expect(formatPercent(0.2)).toBe('20%');
   });
 
-  test('test formatPercent with more settings', () => {
+  test('formatPercent with more settings', () => {
     expect(formatPercent(0.2, 3, true)).toBe('20.000%');
   });
 
-  test('test formatPercent with string', () => {
+  test('formatPercent with string', () => {
     expect(formatPercent('20%')).toBe('20%');
   });
 
-  test('test formatPercent with NaN', () => {
+  test('formatPercent with NaN', () => {
     expect(formatPercent(NaN)).toBe('0%');
   });
 });
 
 describe('test parseFormatedNumber', () => {
-  test('test parseFormatedNumber', () => {
+  test('parseFormatedNumber', () => {
     expect(parseFormatedNumber('123,415,454.23')).toBe(123415454.23);
   });
 });
