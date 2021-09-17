deb8acb945bca457196bf3d5d8d148f005cb723c
Ryan Zhang <eneblue@126.com>
2021-09-17 16:04:57 +0800

    feat(funnel): update funnel storybook and new demo (#49)
    * feat(funnel): update funnel storybook and new demo
    
    * feat(funnel storybook): update funnel multi columns demo
    
    * feat(funnel storybook): update grouped funnel chart
    
    * fix(sonar): modify duplicate code
    
    * feat(storybook): update storybooks
    
    Co-authored-by: Zhang Rui <zhangrui@growingio.com>

--- src/funnel/demos/Funnel.stories.tsx
+++ src/funnel/demos/Funnel.stories.tsx
@@ -6,10 +6,19 @@ import { dataWithMultiBar } from '../../column/demos/data';
 import DrillDownCard from './drilldown-card';
 import { dataWith3Columns, dataWith6Columns, dataWith7Columns, dataWithBasicFunnel, dataWithGroup } from './data';
 
+import Docs from './Funnel.mdx';
+import { formatNumber, formatPercent, InfoCard } from '../..';
+
 export default {
   title: 'Charts/漏斗图 Funnel Chart',
   argTypes: {
     backgroundColor: { control: 'color' },
+  },
+  component: Funnel,
+  parameters: {
+    docs: {
+      page: Docs,
+    },
   },
 };
 
@@ -28,12 +37,126 @@ const config = {
     height: 300,
   },
   scale: ['value', { nice: true }],
+  axises: [
+    [
+      'value',
+      {
+        label: {
+          formatter: (val: string) => formatPercent(val),
+        },
+      },
+    ],
+    ['type', { tickLine: null }],
+  ],
   tooltip: {
     enterable: true,
     showContent: true,
     shared: true,
+    showMarkers: false,
+    render: (options: any) => {
+      return <InfoCard {...options} valueKey="count" />;
+    },
   },
 };
+
+const configLabel = [
+  'value',
+  {
+    content: (data: any) => {
+      return formatNumber(data.count);
+    },
+    type: intervalLabel,
+    style: {
+      fill: '#343434',
+    },
+  },
+];
+
+export const FunnelWith3Columns = Template.bind({});
+const FunnelWith3ColumnsArgs = {
+  legends: [],
+  data: dataWith3Columns,
+  config: {
+    ...config,
+    funnel: {
+      position: 'type*value',
+      // position,
+      color: 'type',
+      label: configLabel,
+    },
+    tooltip: {
+      enterable: true,
+      showContent: true,
+      // shared: true,
+      showMarkers: false,
+      render: (options: any) => {
+        return <DrillDownCard options={options} />;
+      },
+    },
+  },
+};
+FunnelWith3Columns.storyName = '基础漏斗';
+FunnelWith3Columns.args = FunnelWith3ColumnsArgs;
+
+export const FunnelWith6Columns = Template.bind({});
+const FunnelWith6ColumnsArgs = {
+  legends: [],
+  data: dataWith6Columns,
+  config: {
+    ...config,
+    funnel: {
+      position,
+      color: 'type',
+      label: configLabel,
+    },
+  },
+};
+FunnelWith6Columns.storyName = '6个事件漏斗';
+FunnelWith6Columns.args = FunnelWith6ColumnsArgs;
+
+export const FunnelWith7Columns = Template.bind({});
+const FunnelWith7ColumnsArgs = {
+  legends: [],
+  data: dataWith7Columns,
+  config: {
+    ...config,
+    funnel: {
+      position,
+      color: 'type',
+      label: configLabel,
+    },
+  },
+};
+FunnelWith7Columns.storyName = '7个事件漏斗';
+FunnelWith7Columns.args = FunnelWith7ColumnsArgs;
+
+const GroupTemplate: ComponentStory<typeof Funnel> = (args) => (
+  <Card>
+    <GroupedFunnel {...args} />
+  </Card>
+);
+
+export const FunnelWithGroup = GroupTemplate.bind({});
+const FunnelWithGroupArgs = {
+  legends: ['北京', '上海', '南京', '深圳', '广东'],
+  data: dataWithGroup,
+  config: {
+    ...config,
+    funnel: {
+      position,
+      color: 'city',
+      adjust: [
+        {
+          type: 'dodge',
+          marginRatio: 0,
+        },
+      ],
+      label: configLabel,
+    },
+  },
+};
+FunnelWithGroup.storyName = '分组漏斗';
+FunnelWithGroup.args = FunnelWithGroupArgs;
 
 export const FunnelWithLink = Template.bind({});
 const FunnelWithLinkArgs = {
@@ -64,126 +187,3 @@ const FunnelWithBasicArgs = {
 };
 FunnelWithBasic.storyName = '占位柱子';
 FunnelWithBasic.args = FunnelWithBasicArgs;
-
-export const FunnelWith3Columns = Template.bind({});
-const FunnelWith3ColumnsArgs = {
-  legends: [],
-  data: dataWith3Columns,
-  config: {
-    ...config,
-    funnel: {
-      position,
-      color: 'type',
-      label: [
-        'value',
-        {
-          type: intervalLabel,
-          style: {
-            fill: '#343434',
-          },
-        },
-      ],
-    },
-    tooltip: {
-      enterable: true,
-      showContent: true,
-      // shared: true,
-      showMarkers: false,
-      render: (options: any) => {
-        return <DrillDownCard options={options} />;
-      },
-    },
-  },
-};
-FunnelWith3Columns.storyName = '基础漏斗';
-FunnelWith3Columns.args = FunnelWith3ColumnsArgs;
-
-export const FunnelWith6Columns = Template.bind({});
-const FunnelWith6ColumnsArgs = {
-  legends: [],
-  data: dataWith6Columns,
-  config: {
-    ...config,
-    funnel: {
-      position,
-      color: 'type',
-      label: [
-        'value',
-        {
-          type: intervalLabel,
-          style: {
-            fill: '#343434',
-          },
-        },
-      ],
-    },
-  },
-};
-FunnelWith6Columns.storyName = '6个事件漏斗';
-FunnelWith6Columns.args = FunnelWith6ColumnsArgs;
-
-export const FunnelWith7Columns = Template.bind({});
-const FunnelWith7ColumnsArgs = {
-  legends: [],
-  data: dataWith7Columns,
-  config: {
-    ...config,
-    funnel: {
-      position,
-      color: 'type',
-      label: [
-        'value',
-        {
-          type: intervalLabel,
-          style: {
-            fill: '#343434',
-          },
-        },
-      ],
-    },
-  },
-};
-FunnelWith7Columns.storyName = '7个事件漏斗';
-FunnelWith7Columns.args = FunnelWith7ColumnsArgs;
-
-const GroupTemplate: ComponentStory<typeof Funnel> = (args) => (
-  <Card>
-    <GroupedFunnel {...args} />
-  </Card>
-);
-
-export const FunnelWithGroup = GroupTemplate.bind({});
-const FunnelWithGroupArgs = {
-  legends: ['北京', '上海', '南京', '深圳', '广东'],
-  data: dataWithGroup,
-  config: {
-    ...config,
-    tooltip: {
-      enterable: true,
-      showContent: true,
-      // shared: true,
-      showMarkers: false,
-    },
-    funnel: {
-      position,
-      color: 'city',
-      adjust: [
-        {
-          type: 'dodge',
-          marginRatio: 0,
-        },
-      ],
-      label: [
-        'value',
-        {
-          type: intervalLabel,
-          style: {
-            fill: '#343434',
-          },
-        },
-      ],
-    },
-  },
-};
-FunnelWithGroup.storyName = '分组漏斗';
-FunnelWithGroup.args = FunnelWithGroupArgs;
