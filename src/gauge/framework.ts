fd95ecfdbe7a6abd95a7763c60605b2d0bcf6f4e
Ryan Zhang <eneblue@126.com>
2021-11-11 15:12:09 +0800

    fix(Gauge): support update function (#132)
    Co-authored-by: Zhang Rui <zhangrui@growingio.com>

--- src/gauge/framework.ts
+++ src/gauge/framework.ts
@@ -1,35 +1,46 @@
-import { get, subtract } from 'lodash';
+import { get } from 'lodash';
 import { ChartConfig, ChartOptions, ChartType } from '../interfaces';
 import { colors } from '../theme';
-import { renderChart } from '../core/framework';
+import { fetchViewConfig, generateChart } from '../core/framework';
 import { bindGuageCoordination } from '../utils/frameworks/coordinate';
 import { getShapeConfig } from '../utils/tools/configUtils';
 import { gaugeText } from '../utils/frameworks/text';
 import { formatNumber, formatPercent } from '../utils/formatNumber';
+import { Chart, View } from '@antv/g2';
+import { LooseObject } from '@antv/g-base';
 
 export class Gauge {
-  render = (options: ChartOptions, config: ChartConfig) => {
-    const { id, data, dict = {} } = options;
-    if (!id) {
-      return {};
+  chart: Chart | undefined = undefined;
+  view: View | undefined = undefined;
+  options: ChartOptions | undefined = undefined;
+
+  update = (charts: { chart: Chart }, data: LooseObject[], config: ChartConfig) => {
+    this.view?.clear();
+    console.log(data, config);
+    if (this.view) {
+      this.rednerView(this.view, data, config);
     }
-    const chart = renderChart(options, config);
-    bindGuageCoordination(chart);
+    this.view?.render(true);
+    this.chart?.render(true);
+  };
+
+  rednerView = (view: View, data: LooseObject[], config: ChartConfig) => {
+    const { dict = {} } = this.options || {};
     const gaugeCfg = getShapeConfig(config, ChartType.GAUGE);
     const { position, title, subTitle } = gaugeCfg;
 
     const value = get(data, `[0].${position}`);
     const max = get(data, '[0].max');
 
-    chart.scale(position, {
+    view.line().position(`${position}*1`).color('#00000000');
+    view.scale(position, {
       min: 0,
       max: max || 1,
     });
 
-    chart.axis(position, false);
-    chart.line().position(`${position}*1`).color('#00000000');
+    view.axis(position, false);
     // 绘制背景
-    chart.annotation().arc({
+    view.annotation().arc({
       top: false,
       start: [0, 1],
       end: [max || 1, 1],
@@ -40,7 +51,7 @@ export class Gauge {
       },
     });
     // 绘制指标
-    chart.annotation().arc({
+    view.annotation().arc({
       start: [0, 1],
       end: [value || 0, 1],
       style: {
@@ -53,12 +64,31 @@ export class Gauge {
     const rate = max ? formatPercent(value / max) : '--';
 
     const fixedSubTitle = typeof subTitle === 'number' ? formatNumber(subTitle) : subTitle;
-    gaugeText(title || `${dict.rate}${rate}`, fixedSubTitle || formatNumber(value), chart, config);
+    gaugeText(title || `${dict.rate}${rate}`, fixedSubTitle || formatNumber(value), view, config);
+  };
+
+  render = (options: ChartOptions, config: ChartConfig) => {
+    this.options = options;
+    const { id, data } = options;
+    if (!id) {
+      return {};
+    }
+    const chart = generateChart(options, config);
+
+    const view = chart.createView();
+    fetchViewConfig(view, options, config);
+    bindGuageCoordination(view);
+
+    this.rednerView(view, data as LooseObject[], config);
+
+    view.render();
+    this.view = view;
 
     chart.tooltip(false);
     chart.legend(false);
     chart.render();
-    return { chart };
+    this.chart = chart;
+    return { chart, update: this.update };
   };
   legend = () => {};
 }
