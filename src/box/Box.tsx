99243efdf5d7b21af77eff1aa0042383b36398ed
Zhang Rui <zhangrui@growingio.com>
2023-01-29 14:37:37 +0800

    fix: resolve conflict of box chart

--- src/box/Box.tsx
+++ src/box/Box.tsx
@@ -9,28 +9,32 @@ import { hasDodge } from '../utils/interval';
 
 export interface BoxProps extends ChartProps {
   config: BoxConfig;
-  size?: 'normal' | 'small'
+  size?: 'normal' | 'small';
 }
 
 const Box: React.FC<BoxProps> = (props: BoxProps) => {
   const { data, legends: legendProps = [], title, config, size = 'normal' } = props;
   const isDodge = useMemo(() => hasDodge(config?.['box']), [config]);
 
-  const customSizeConfig = useMemo(() => 
-    size === 'small' ? {
-      intervalPadding: isDodge ? 30 : 12,
-      dodgePadding: 16,
-      columnWidth: 16,
-      maxColumnWidth: 20,
-      minColumnWidth: 16,
-    } : {
-      intervalPadding: isDodge ? 60 : 16,
-      dodgePadding: 16,
-      columnWidth: 16,
-      maxColumnWidth: 60,
-      minColumnWidth: 16,
-    }
-  , [size, isDodge]);
+  const customSizeConfig = useMemo(
+    () =>
+      size === 'small'
+        ? {
+            intervalPadding: isDodge ? 30 : 12,
+            dodgePadding: 16,
+            columnWidth: 16,
+            maxColumnWidth: 20,
+            minColumnWidth: 16,
+          }
+        : {
+            intervalPadding: isDodge ? 60 : 16,
+            dodgePadding: 16,
+            columnWidth: 16,
+            maxColumnWidth: 60,
+            minColumnWidth: 16,
+          },
+    [size, isDodge]
+  );
 
   const [box] = useState(new BoxCls());
   config.type = ChartType.BOX;
@@ -56,13 +60,18 @@ const Box: React.FC<BoxProps> = (props: BoxProps) => {
       return {
         defaultStyles: {
           box: (legend: any) => ({
-            stroke: colors[legendProps.findIndex((l) => (l?.name ?? l) === (legend?.name ?? legend)) % colors.length],
-            fill: colors[legendProps.findIndex((l) => (l?.name ?? l)  === (legend?.name ?? legend)) % colors.length],
+            stroke:
+              colors[legendProps.findIndex((l: any) => (l?.name ?? l) === (legend?.name ?? legend)) % colors.length],
+            fill: colors[
+              legendProps.findIndex((l: any) => (l?.name ?? l) === (legend?.name ?? legend)) % colors.length
+            ],
             fillOpacity: 0.2,
           }),
           point: (legend: any) => ({
             lineWidth: 0,
-            fill: colors[legendProps.findIndex((l) => (l?.name ?? l)  === (legend?.name ?? legend)) % colors.length],
+            fill: colors[
+              legendProps.findIndex((l: any) => (l?.name ?? l) === (legend?.name ?? legend)) % colors.length
+            ],
           }),
         },
       };
@@ -77,8 +86,9 @@ const Box: React.FC<BoxProps> = (props: BoxProps) => {
       defaultOptions={defaultOptions}
       legendList={legendProps}
       config={config}
-      callChart={box.render}
-      handleLegend={box.legend}
+      // callChart={box.render}
+      // handleLegend={box.legend}
+      chart={box}
     />
   );
 };
