15727ff8484381b70627a75976d3f980ba035582
vermilionAnd <vermilionAnd@gmail.com>
2023-01-29 15:57:26 +0800

    fix: bug

--- src/combine-bar/demos/Bar.stories.tsx
+++ src/combine-bar/demos/Bar.stories.tsx
@@ -49,8 +49,84 @@ const labelConfig = [
 
 export const BarDefault = Template.bind({});
 const BarDefaultArgs = {
-  legends: [],
-  data: dataWithMulti,
+  legends: [
+    { name: '目标群组', color: '#5F87FF', pointColor: '#1248E9' },
+    { name: '目标群组TGI', alias: '目标群组TGI', color: '#1248E9', shapeType: 'circle' },
+    { name: '对照组', color: '#FFDD63', pointColor: '#FAA937' },
+    { name: '对照组TGI', alias: '对照组TGI', color: '#FAA937', shapeType: 'circle' },
+  ],
+  data: [
+    {
+        "dimensionValue": "崔宜",
+        "userCount": 59,
+        "tgi": 547.7632,
+        "name": "用户量",
+        "group": "目标群组"
+    },
+    {
+        "dimensionValue": "崔宜",
+        "userCount": 59,
+        "tgi": 102.58748,
+        "name": "用户量",
+        "group": "对照组"
+    },
+    {
+        "dimensionValue": "LoganCarpenter",
+        "userCount": 59,
+        "tgi": 547.7632,
+        "name": "用户量",
+        "group": "目标群组"
+    },
+    {
+        "dimensionValue": "LoganCarpenter",
+        "userCount": 59,
+        "tgi": 102.58748,
+        "name": "用户量",
+        "group": "对照组"
+    },
+    {
+        "dimensionValue": "涂友娜",
+        "userCount": 56,
+        "tgi": 547.7632,
+        "name": "用户量",
+        "group": "目标群组"
+    },
+    {
+        "dimensionValue": "涂友娜",
+        "userCount": 56,
+        "tgi": 102.58748,
+        "name": "用户量",
+        "group": "对照组"
+    },
+    {
+        "dimensionValue": "闵燕",
+        "userCount": 53,
+        "tgi": 547.7632,
+        "name": "用户量",
+        "group": "目标群组"
+    },
+    {
+        "dimensionValue": "闵燕",
+        "userCount": 53,
+        "tgi": 102.58748,
+        "name": "用户量",
+        "group": "对照组"
+    },
+    {
+        "dimensionValue": "AliceVaughn",
+        "userCount": 51,
+        "tgi": 547.7632,
+        "name": "用户量",
+        "group": "目标群组"
+    },
+    {
+        "dimensionValue": "AliceVaughn",
+        "userCount": 51,
+        "tgi": 102.58748,
+        "name": "用户量",
+        "group": "对照组"
+    }
+],
   config: {
     ...config,
     chart: {
@@ -58,14 +134,15 @@ const BarDefaultArgs = {
       height: 300,
     },
     bar: {
-      position: 'name*value',
-      color: 'name',
+      position: 'dimensionValue*userCount',
+      color: 'group',
+      adjust: [{ type: 'dodge' }],
       label: labelConfig,
     },
-    axises: [['value',false],['tgi',false]],
+    axises: [['userCount',false],['tgi',false]],
     point:{
-      position:'name*tgi',
-      color:'#1248E9',
+      position:'dimensionValue*tgi',
+      color: 'group',
       shape:'circle',
       size:8
     },
