11389f9a55e2fb475c2ec0ddc3ceb589adfb6bb6
Zhang Rui <zhangrui@growingio.com>
2023-04-04 14:47:59 +0800

    fix: 修改分割线图的单个点时的样式

--- src/utils/tools/shapes/lineSplitLine.ts
+++ src/utils/tools/shapes/lineSplitLine.ts
@@ -1,6 +1,7 @@
 import { Point } from '@antv/g-base';
 import { IGroup, registerShape } from '@antv/g2';
 import { ShapeInfo, ShapeVertices } from '@antv/g2/lib/interface';
+import { isEmpty } from 'lodash';
 
 const splitData = (points: ShapeVertices, data?: any) => {
   let marker = 0;
@@ -22,50 +23,86 @@ const splitData = (points: ShapeVertices, data?: any) => {
       points2.push(d);
     }
   }
-  points1.push(points2[0]);
-
+  if (points2[0]) {
+    points1.push(points2[0]);
+  }
   return [points1, points2];
 };
 
 registerShape('line', 'split-line', {
   draw(cfg: ShapeInfo, container: IGroup) {
     const group = container.addGroup();
-    const pointArrs = splitData(cfg.points as ShapeVertices, cfg.data);
-    const path1 = [];
-    for (let i = 0; i < pointArrs[0].length; i++) {
-      let pre = 'L';
-      if (i === 0) {
-        pre = 'M';
-      }
-      path1.push([pre, (pointArrs[0][i] as Point)?.x, (pointArrs[0][i] as Point)?.y]);
-    }
-    group.addShape('path', {
-      attrs: {
-        path: path1,
-        // cfg?.style?.stroke 为通过style()设定的stroke颜色
-        // cfg.color 为默认的颜色
-        stroke: cfg?.style?.stroke || cfg.color,
-        lineWidth: 2,
-      },
-    });
+    const [solidLinePoints, dashLinePoints] = splitData(cfg.points as ShapeVertices, cfg.data);
 
-    const path2 = [];
-    for (let i = 0; i < pointArrs[1].length; i++) {
-      let pre = 'L';
-      if (i === 0) {
-        pre = 'M';
+    if (solidLinePoints.length === 1) {
+      group.addShape('circle', {
+        attrs: {
+          x: (solidLinePoints[0] as Point)?.x,
+          y: (solidLinePoints[0] as Point)?.y,
+          r: 2,
+          // cfg?.style?.stroke 为通过style()设定的stroke颜色
+          // cfg.color 为默认的颜色
+          stroke: cfg?.style?.stroke || cfg.color,
+          fill: cfg?.style?.stroke || cfg.color,
+          lineWidth: 2,
+        },
+      });
+    } else if (solidLinePoints.length > 1) {
+      const solidPath = [];
+      for (let i = 0; i < solidLinePoints.length; i++) {
+        let pre = 'L';
+        if (i === 0) {
+          pre = 'M';
+        }
+        if (!solidLinePoints[i]) {
+          if (i === 1) {
+            group.addShape('circle', {
+              attrs: {
+                x: (solidLinePoints[0] as Point)?.x,
+                y: (solidLinePoints[0] as Point)?.y,
+                r: 2,
+                // cfg?.style?.stroke 为通过style()设定的stroke颜色
+                // cfg.color 为默认的颜色
+                stroke: cfg?.style?.stroke || cfg.color,
+                fill: cfg?.style?.stroke || cfg.color,
+                lineWidth: 2,
+              },
+            });
+          }
+        } else {
+          solidPath.push([pre, (solidLinePoints[i] as Point)?.x, (solidLinePoints[i] as Point)?.y]);
+        }
       }
-      path2.push([pre, (pointArrs[1][i] as Point).x, (pointArrs[1][i] as Point).y]);
+      group.addShape('path', {
+        attrs: {
+          path: solidPath,
+          // cfg?.style?.stroke 为通过style()设定的stroke颜色
+          // cfg.color 为默认的颜色
+          stroke: cfg?.style?.stroke || cfg.color,
+          lineWidth: 2,
+        },
+      });
     }
-    group.addShape('path', {
-      attrs: {
-        path: path2,
-        stroke: cfg?.style?.stroke || cfg.color,
-        lineWidth: 2,
-        lineDash: [5, 2],
-        opacity: 0.7,
-      },
-    });
+
+    if (dashLinePoints.length > 0) {
+      const dashPath = [];
+      for (let i = 0; i < dashLinePoints.length; i++) {
+        let pre = 'L';
+        if (i === 0) {
+          pre = 'M';
+        }
+        dashPath.push([pre, (dashLinePoints[i] as Point).x, (dashLinePoints[i] as Point).y]);
+      }
+      group.addShape('path', {
+        attrs: {
+          path: dashPath,
+          stroke: cfg?.style?.stroke || cfg.color,
+          lineWidth: 2,
+          lineDash: [5, 2],
+          opacity: 0.7,
+        },
+      });
+    }
 
     return group;
   },
