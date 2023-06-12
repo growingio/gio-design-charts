1d3e39492a7d5625a7027db6e9878ce06a34dfe0
Ryan Zhang <eneblue@126.com>
2023-06-12 10:23:56 +0800

    feat(DoubleAxes): 支持双轴图表 (#357)
    * feat(DoubleAxes): 支持双轴
    
    * fix: 移除console.log

---
+++ src/double-axes/demos/data.ts
@@ -0,0 +1,14 @@
+export const data = [
+  { time: '2023-03-25', label: '参与人数', col: 100, lineLabel: '参与率(%)', lineValue: 30 },
+  { time: '2023-03-25', label: '触发人数', col: 200, lineLabel: '出发率(%)', lineValue: 70 },
+  { time: '2023-03-26', label: '参与人数', col: 200, lineLabel: '参与率(%)', lineValue: 40 },
+  { time: '2023-03-26', label: '触发人数', col: 300, lineLabel: '出发率(%)', lineValue: 60 },
+  { time: '2023-03-27', label: '参与人数', col: 300, lineLabel: '参与率(%)', lineValue: 60 },
+  { time: '2023-03-27', label: '触发人数', col: 200, lineLabel: '出发率(%)', lineValue: 20 },
+  { time: '2023-03-28', label: '参与人数', col: 300, lineLabel: '参与率(%)', lineValue: 60 },
+  { time: '2023-03-28', label: '触发人数', col: 200, lineLabel: '出发率(%)', lineValue: 20 },
+  { time: '2023-03-29', label: '参与人数', col: 300, lineLabel: '参与率(%)', lineValue: 60 },
+  { time: '2023-03-29', label: '触发人数', col: 200, lineLabel: '出发率(%)', lineValue: 20 },
+  { time: '2023-03-30', label: '参与人数', col: 300, lineLabel: '参与率(%)', lineValue: 60 },
+  { time: '2023-03-30', label: '触发人数', col: 200, lineLabel: '出发率(%)', lineValue: 20 },
+];
---
+++ src/double-axes/index.tsx
@@ -0,0 +1,2 @@
+export type { DoubleAxesProps } from './DoubleAxes';
+export { default, default as DoubleAxes } from './DoubleAxes';
