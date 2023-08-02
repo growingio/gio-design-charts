e20e23c6b8c9296fb330a5b7e1150e40e56e0d31
Zhang Rui <eneblue@126.com>
2023-08-02 18:04:03 +0800

    fix(funnel): 修改漏斗图最下高度

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,11 @@
 import { format } from 'date-fns';
-import { zhCN } from 'date-fns/locale';
+import { zhCN, enUS } from 'date-fns/locale';
 
 export const formatDateByTs = (text: string | number) =>
-  !isNaN(Number(text)) ? format(new Date(Number(text)), 'MM/dd EEE', { locale: zhCN }) : text;
+  !isNaN(Number(text))
+    ? format(new Date(Number(text)), 'MM/dd EEE', {
+        locale: localStorage.getItem('locale') === 'en-US' ? enUS : zhCN,
+      })
+    : text;
 
 export default formatDateByTs;
